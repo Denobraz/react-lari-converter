749dd3b2fedb2d2b059b82a65c70b5556f8ebc04
Denis Shevchenko <webmaster@gartal.cz>
2025-08-14 12:23:17 +0300

    Add gh pages support

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/react-lari-converter/', // для GitHub Pages (repo name)
+  build: { outDir: 'build' }, // чтобы gh-pages -d build нашёл каталог
   plugins: [react(), tailwindcss()],
 })

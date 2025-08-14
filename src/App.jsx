c962b9052892c50f4fddab4228348f6adce85153
Denis Shevchenko <webmaster@gartal.cz>
2025-08-14 22:13:37 +0300

    add localStorage usage for save payments

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import PaymentForm from './components/PaymentForm.jsx';
 import PaymentsTable from './components/PaymentsTable.jsx';
 import {Readme} from "./components/Readme.jsx";
 
 function App() {
-    const [payments, setPayments] = useState([]);
+    const [payments, setPayments] = useState(() => {
+        const saved = localStorage.getItem('payments');
+        return saved ? JSON.parse(saved) : [];
+    });
     const addPayment = (p) => setPayments(prev => [...prev, p]);
     const handleDelete = (idx) => setPayments(prev => prev.filter((_, i) => i !== idx));
+
+    useEffect(() => {
+        localStorage.setItem('payments', JSON.stringify(payments));
+    }, [payments]);
 
     return (
         <div className='min-h-screen w-full flex flex-col justify-center bg-black text-white px-6 py-5'>

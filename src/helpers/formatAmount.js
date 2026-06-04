19a795ceae70347e86b7cbb19da3fb84920a6682
Denis Shevchenko <webmaster@gartal.cz>
2026-06-04 14:26:36 +0300

    Updates

--- src/helpers/formatAmount.js
+++
@@ -1,6 +0,0 @@
-export function formatAmount(amount) {
-    return new Intl.NumberFormat('ru-RU', {
-        style: 'decimal',
-        maximumFractionDigits: 2,
-    }).format(amount).replace(',', '.');
-}
